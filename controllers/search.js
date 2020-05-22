a9d462760ff3cf0f6f3943e19fea4934e4de392f
Digvijay Singh Rathore <dynamcidigvijay@gmail.com>
2020-05-22 16:34:29 +0530

    Crash Error Fixed

--- controllers/search.js
+++ controllers/search.js
@@ -6,19 +6,35 @@ router.post("/search", (req, res) => {
     const search = req.body.keyword
     const colors = req.body.color
     const orientation = req.body.orientation
-    fetch(`https://api.unsplash.com/search/photos/?query=${search}&color=${colors}&orientation=${orientation}&client_id=dxB1SfFCctr5KEHfBbBXL4KJ7EqM2RwxQDUWUkYMsFw`)
-    .then(res => res.json())
-    .then((json) => {
-        const variable = json.results[0]
-        console.log(variable)
-        if(variable === undefined){
-            return res.status(200).render("error")
-        } else {
-            const image0 = json.results[0].urls.small
-            const image1 = json.results[1].urls.small
-            return res.status(200).render("search", {data: [image0, image1]})
-        }
-    })
+    if(orientation === undefined || colors === undefined){
+        fetch(`https://api.unsplash.com/search/photos/?query=${search}&client_id=dxB1SfFCctr5KEHfBbBXL4KJ7EqM2RwxQDUWUkYMsFw`)
+        .then(res => res.json())
+        .then((json) => {
+            const variable = json.results[0]
+            console.log(variable)
+            if(variable === undefined){
+                return res.status(200).render("error")
+            } else {
+                const image0 = json.results[0].urls.small
+                const image1 = json.results[1].urls.small
+                return res.status(200).render("search", {data: [image0, image1]})
+            }
+        })
+    } else {
+        fetch(`https://api.unsplash.com/search/photos/?query=${search}&color=${colors}&orientation=${orientation}&client_id=dxB1SfFCctr5KEHfBbBXL4KJ7EqM2RwxQDUWUkYMsFw`)
+        .then(res => res.json())
+        .then((json) => {
+            const variable = json.results[0]
+            console.log(variable)
+            if(variable === undefined){
+                return res.status(200).render("error")
+            } else {
+                const image0 = json.results[0].urls.small
+                const image1 = json.results[1].urls.small
+                return res.status(200).render("search", {data: [image0, image1]})
+            }
+        })
+    }
 })
 
 module.exports = router;
