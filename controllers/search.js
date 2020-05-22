8f9bb4a7ae470547368aa794e25cf16a85aac80a
Digvijay Singh Rathore <dynamcidigvijay@gmail.com>
2020-05-22 16:08:55 +0530

    Crash Error Fixed

--- controllers/search.js
+++ controllers/search.js
@@ -5,15 +5,19 @@ const fetch = require("node-fetch");
 router.post("/search", (req, res) => {
     const search = req.body.keyword
     const colors = req.body.color
-    fetch(`https://pixabay.com/api/?key=16669259-11ed0855b28ae24ce05239d29&q=${search}&colors=${colors}&image_type=photo`)
+    const orientation = req.body.orientation
+    fetch(`https://api.unsplash.com/search/photos/?query=${search}&color=${colors}&orientation=${orientation}&client_id=dxB1SfFCctr5KEHfBbBXL4KJ7EqM2RwxQDUWUkYMsFw`)
     .then(res => res.json())
     .then((json) => {
-        const image0 = json.hits[0].webformatURL
-        const image1 = json.hits[1].webformatURL
-        const image2 = json.hits[2].webformatURL
-        const image3 = json.hits[3].webformatURL
-        const image4 = json.hits[4].webformatURL
-        return res.status(200).render("search", {data: [image0, image1, image2, image3, image4]})
+        const variable = json.results
+        console.log(variable)
+        if(variable === undefined){
+            return res.status(200).render("error")
+        } else {
+            const image0 = json.results[0].urls.small
+            const image1 = json.results[1].urls.small
+            return res.status(200).render("search", {data: [image0, image1]})
+        }
     })
 })
 
